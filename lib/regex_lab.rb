1c85f1e6749250eb84e7b7e6dd8a357ae2a6a12f
+ <+>
2020-11-29 08:32:14 -0700

    commit tree fe74aba03502f2b2235e5bf2849b4d39b825aa71

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,24 @@
-def starts_with_a_vowel?(word)
 
+
+
+def starts_with_a_vowel?(word)
+    word.scan(/\b[aeiou]/) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.count(/\bun\w+ing/) ? true : false
 end
 
 def words_five_letters_long(text)
-
+    words = []
+    words << text.count(/\b[a-z]{5}\b/)
+    words
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    text.scan(/\b\w+[!?.]/) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+    phone.scan(/\d{3}?\.\d{3}?\.\d{4}/) ? true : false
 end
